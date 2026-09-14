e79e35cf250515d3517425f8d227f4f829f18348
Romain Sessa <19792764+romainsessa@users.noreply.github.com>
2026-09-14 15:08:56 +0200

    Enhance Employee class with greeting and comments
    Added a print statement for employee greeting and clarified comments.

--- invalid-srp/src/main/java/fr/visiplus/invalidsrp/Employee.java
+++ invalid-srp/src/main/java/fr/visiplus/invalidsrp/Employee.java
@@ -4,13 +4,14 @@ public class Employee {
 	
 	private int daysPerWeek = 5;
 	private int hoursPerDay = 7;
-	private int workedHoursPerDay = 8;
+	private int workedHoursPerDay = 8; // this a new needed information
 	
 	private int calculateHoursPerWeek() {
-		return daysPerWeek * workedHoursPerDay;
+		return daysPerWeek * workedHoursPerDay; // process
 	}
 	
 	public int provideToEmployee() {
+		System.out.prinln("Hello employee");
 		return calculateHoursPerWeek();
 	}
 	
